25bd0edf75863e0554a093ee24a8890a21f9aaed
배연준 <49051494+yjbae3974@users.noreply.github.com>
2024-11-10 17:04:08 +0900

    init commit

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BoardsModule } from './boards/boards.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'nestuser',
+      password: 'nestpassword',
+      database: 'nestdb',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // 개발 환경에서만 true로 설정하여 테이블 자동 생성 위험한 옵션이긴 함. 실제 서비스에서는 false로 설정
+    }),
+    BoardsModule,
+    UsersModule,
+    AuthModule,
+  ],
+})
+export class AppModule {}
---
+++ src/boards/dto/create-board.dto.ts
@@ -0,0 +1,9 @@
+export class CreateBoardDto {
+  title: string;
+  content: string;
+}
+
+//DTO : Data Transfer Object
+//DTO는 데이터 전송 객체로, 데이터를 전송하기 위한 객체를 의미합니다.
+//DTO는 데이터베이스와 통신하는 서비스나 레이어 사이에서 데이터를 전달하는 역할을 합니다.
+//DTO는 데이터를 전달하는 역할을 하기 때문에 데이터 전송을 위한 객체를 의미합니다.
